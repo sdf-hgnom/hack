fc82441f4fbb272663fff51ee32ca87b013b2ce6
sdf-hgnom <shulinab@yandex.ru>
2020-11-17 06:42:25 +0800

    finish version

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ import requests
 
 N = 10
 
-urls = ['https://www.speedtest.net/',
-        'https://avito.ru/',
+
+urls = ['https://www.avito.ru/',
+        'https://www.speedtest.net/',
         'https://djbook.ru/',
         'http://styxcosmetic.ru/',
         'https://www.simplifiedpython.net/',
@@ -15,14 +16,25 @@ urls = ['https://www.speedtest.net/',
         ]
 
 
-def test_url(what: Text, count: int = 10):
+def test_url(what_url: Text, count: int = 10, sleep_time: int = 1):
+    """
+    выполнит http get запрос по  адресу если код не 200 сгенирит ошибку
+    если все хорошо - подсчитает время выполнения запросов
+    :param what_url: url запроса
+    :param count: кол-во повторов
+    :param sleep_time: задержка между повторами
+    :return:
+    """
     begin_time = time.monotonic()
-    print(f'\nbegin test {what} :')
+    print(f'\nbegin test {what_url} :')
     for i in range(count):
-        print('.', end='')
-        response = requests.get(what)
+        response = requests.get(url=what_url)
+        print('.',end='')
         if response.status_code != 200:
-            raise ConnectionError(f'{what} -> Bad response code {response.status_code} in {i} iteration')
+            raise ConnectionError(f'{what_url} -> Bad response code {response.status_code} in {i} iteration ')
+
+        time.sleep(sleep_time)
+
     return time.monotonic() - begin_time
 
 
@@ -42,7 +54,7 @@ def get_pass(len: int = 8):
 if __name__ == '__main__':
     for url in urls:
         try:
-            time_period = test_url(what=url, count=10)
+            time_period = test_url(what_url=url, count=10, sleep_time=5)
             print(f'\nTest end with {time_period} sec')
         except ConnectionError as ex:
             print(ex.args)
